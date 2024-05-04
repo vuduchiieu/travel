79d1ffc3d7a38960f2503d2249782f16e818d1f4
vuduchiieu <vuduchiieu@gmail.com>
2024-05-05 01:02:37 +0700

    fix loi 2

--- src/assets/icon/icon.ts
+++ src/assets/icon/icon.ts
@@ -6,6 +6,8 @@ const icon: {
   search: string;
   user: string;
   heart: string;
+  eye: string;
+  eye_hidden: string;
 } = {
   arrow_r: require("./arrow-right.svg").default.src,
   logo: require("./logo.svg").default.src,
@@ -14,7 +16,8 @@ const icon: {
   search: require("./search.svg").default.src,
   user: require("./user.svg").default.src,
   heart: require("./heart.svg").default.src,
-
+  eye: require("./eye.svg").default.src,
+  eye_hidden: require("./eye_hidden.svg").default.src,
 };
 
 export default icon;
