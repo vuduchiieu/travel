3559d30ccefa21876b6794444f9eb4e1ecc63f1e
vuduchiieu <vuduchiieu@gmail.com>
2024-05-05 23:47:02 +0700

    test newffeed

--- src/assets/picture/image.ts
+++ src/assets/picture/image.ts
@@ -2,10 +2,14 @@ const image: {
   instagram: string;
   acc1: string;
   post1: string;
+  post2: string;
+  post3: string;
 } = {
   instagram: require("./instagramIcon.jpg").default.src,
   acc1: require("./acc1.jpg").default.src,
   post1: require("./post1.jpg").default.src,
+  post2: require("./post2.jpg").default.src,
+  post3: require("./post3.jpg").default.src,
 };
 
 export default image;
