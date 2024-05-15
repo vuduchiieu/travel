f8293e073a8b2d59ac84a8bd433c092f449afd1e
vuduchiieu <vuduchiieu@gmail.com>
2024-05-15 08:18:34 +0700

    newloading

--- src/app/[id]/loading.tsx
+++ src/app/[id]/loading.tsx
@@ -1,17 +1,21 @@
 import icon from "@/assets/icon/icon";
+import Header from "@/components/Header/Header";
 import Image from "next/image";
 
 export default function Loading() {
   return (
-    <div className="flex justify-center items-center w-screen h-screen">
-      <Image
-        width={32}
-        height={32}
-        priority
-        className="animate-spin"
-        src={icon.loading}
-        alt=""
-      />
-    </div>
+    <main className="w-screen h-screen">
+      <Header />
+      <div className="flex justify-center items-center w-[100%] h-[100%]">
+        <Image
+          width={32}
+          height={32}
+          priority
+          className="animate-spin"
+          src={icon.loading}
+          alt=""
+        />
+      </div>
+    </main>
   );
 }
